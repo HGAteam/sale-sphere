6a7e51148a5092a8dd42b9261d1b1e0ccef771b8
Hector Gabriel Adrián Tula <adrian_tula@hotmail.com>
2024-04-01 22:39:44 -0300

    ec-vendor change to ec-cat style

--- resources/views/pages/admin/suppliers/index.blade.php
+++ resources/views/pages/admin/suppliers/index.blade.php
@@ -17,7 +17,7 @@
 
     <div class="row">
         <div class="col-12">
-            <div class="ec-vendor-list card card-default">
+            <div class="ec-cat-list card card-default">
                 <div class="card-header py-0">
                     <div class="card-title">
                         <h1>{{__('Supplier List')}}</h1>
